c213e4a2f6d56091fd63c6050c74a2ced82a6c4a
Filippo Tosetto <filippo@Filippos-MacBook-Pro.local>
2016-02-02 10:55:13 +0000

    Added support for Carthage

---
+++ SwiftOpenWeatherMapAPI/SwiftOpenWeatherMapApi.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOpenWeatherMapApi.h
+//  SwiftOpenWeatherMapApi
+//
+//  Created by Filippo Tosetto on 02/02/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftOpenWeatherMapApi.
+FOUNDATION_EXPORT double SwiftOpenWeatherMapApiVersionNumber;
+
+//! Project version string for SwiftOpenWeatherMapApi.
+FOUNDATION_EXPORT const unsigned char SwiftOpenWeatherMapApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOpenWeatherMapApi/PublicHeader.h>
+
+
